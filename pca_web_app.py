260c1f02edce4b09bebb80b413c6ff63a95f8971
Webb-Lab <ikwebb@iu.edu>
2026-04-28 12:04:11 -0400

    Update pca_web_app.py
    fixing plot appearance

--- pca_web_app.py
+++ pca_web_app.py
@@ -117,17 +117,22 @@ if uploaded_file:
         font=dict(color="black"),
     
         xaxis=dict(
+            showgrid=False,
+            zeroline=False,
             title_font=dict(color="black"),
             tickfont=dict(color="black"),
             color="black"
         ),
         yaxis=dict(
+            showgrid=False,
+            zeroline=False,
             title_font=dict(color="black"),
             tickfont=dict(color="black"),
             color="black"
         ),
     
         legend=dict(
+            title_font=dict(color="black"),
             font=dict(color="black")
         )
     )
@@ -178,6 +183,10 @@ if uploaded_file:
         hover_data=["Crosslink"]
     )
 
+    fig.add_vline(x=fc_thresh)
+    fig.add_vline(x=-fc_thresh)
+    fig.add_hline(y=-np.log10(q_thresh))
+
     fig.update_layout(
         plot_bgcolor="white",
         paper_bgcolor="white",
@@ -185,11 +194,13 @@ if uploaded_file:
         font=dict(color="black"),
     
         xaxis=dict(
+            showgrid=False,
             title_font=dict(color="black"),
             tickfont=dict(color="black"),
             color="black"
         ),
         yaxis=dict(
+            showgrid=False
             title_font=dict(color="black"),
             tickfont=dict(color="black"),
             color="black"
@@ -197,12 +208,11 @@ if uploaded_file:
     
         legend=dict(
             font=dict(color="black")
+            title_font=dict(color="black")
         )
     )
 
-    fig.add_vline(x=fc_thresh)
-    fig.add_vline(x=-fc_thresh)
-    fig.add_hline(y=-np.log10(q_thresh))
+    
 
     st.plotly_chart(fig)
 
